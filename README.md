50158e2fb061d4287e45d84089ccb7e04b4aa5e3
1noseA <1noseA2L@gmail.com>
2023-05-05 23:52:20 +0900

    フォームの追加

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# vue-todo
+# vue-Todo
 
 ## Project setup
 ```
---
+++ src/components/Todo.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2>Todo List</h2>
+      <form v-on:submit.prevent>
+        <input type="text" v-model="newItem">
+        <button v-on:click="addItem">Add</button>
+      </form>
+      <pre>{{ $data }}</pre>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Todo',
+  data() {
+    return {
+      newItem: ''
+    }
+  },
+  methods: {
+    // eslint-disable-next-line no-unused-vars
+    addItem: function(event) {
+      console.log('Clicked!')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
